ce807f2287b588c29f9de4105579b87b4a769843
Brett Yarrow <byarrow86@gmail.com>
2019-11-15 11:38:28 -0800

    added randomly getting 15 characers from api

--- src/api/api.js
+++ src/api/api.js
@@ -9,28 +9,48 @@ const superheroUrl = id =>
 
 export default class Api extends Component {
     state = {
-        heroData: {},
+        heroData: [],
         loading: true
     };
     componentDidMount() {
-        axios.get(superheroUrl(370)).then(response => {
-            this.setState({
-                heroData: response.data,
-                loading: false
-            });
-        });
+        let idMax = 731;
+        let rand;
+        let found;
+        for (let i = 0; i < 15; i++) {
+            rand = Math.floor((Math.random() * (idMax - i)) + 1);
+            console.log(rand);
+            found = this.state.heroData.some(hero => hero.id === rand);
+            console.log(found)
+            if (!found) {
+                axios.get(superheroUrl(rand)).then(response => {
+                    this.setState(prevState => ({
+                        heroData: [...prevState.heroData, response.data]
+                    }), () => {
+                        if (i === 14) {
+                            this.setState({
+                                loading: false
+                            });
+                        }
+                    })
+                });
+            } else {
+                i--;
+            }
+        }
+
+
     }
 
     render() {
-        if (this.state.loading) return <div>Loading.....</div>;
+        if (this.state.loading) { return <div>Loading.....</div>; }
         console.log(this.state.heroData)
         return (
             <>
-                <p>Name: {this.state.heroData.name}</p>
+                <p>Name: {this.state.heroData[0].name}</p>
                 {/* <p>Name: {this.state.heroData.biography['full-name']}</p> */}
-                <p>Height: {this.state.heroData.appearance.height[0]}</p>
-                <p>Weight: {this.state.heroData.appearance.weight[0]}</p>
-                <img src={this.state.heroData.image.url}></img>
+                <p>Height: {this.state.heroData[0].appearance.height[0]}</p>
+                <p>Weight: {this.state.heroData[0].appearance.weight[0]}</p>
+                <img src={this.state.heroData[0].image.url}></img>
             </>
         )
     }
